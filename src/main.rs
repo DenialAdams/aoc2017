4c7a8ae3924f66d35e93f3c8544226b49dad8d6e
Richard McCormack <brick@brick.codes>
2017-12-03 15:31:51 -0500

    day3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(inclusive_range_syntax)]
+
 extern crate itertools;
 
 use itertools::Itertools;
@@ -19,6 +21,8 @@ fn main() {
         "{:?}",
         corruption_checksum_part_two(include_str!("day2.in").trim_right())
     );
+    println!("{}", spiral_memory_part_one(265149));
+    println!("{}", spiral_memory_part_two(265149));
 }
 
 pub fn inverse_captcha_part_one(captcha: &[u8]) -> Result<u64, &'static str> {
@@ -93,6 +97,88 @@ pub fn corruption_checksum_part_two(spreadsheet: &str) -> Result<u64, ::std::num
     Ok(sum)
 }
 
+fn spiral_memory_part_one(start: i64) -> i64 {
+    // TODO: we should reject negative numbers
+    // Biggest number in ring
+    let mut sum = 1;
+    // Which ring we are in
+    let mut n = 0;
+    while sum < start {
+        n += 1;
+        sum += 8 * n;
+    }
+    // Distance is our ring number (n) + number of tiles we are away from the closest center
+    let centers = vec![sum - (n * 1), sum - (n * 3), sum - (n * 5), sum - (n * 7)];
+    let mut closest_center_dist = ::std::i64::MAX;
+    for center in centers.into_iter() {
+        let dist = (start - center).abs();
+        if dist < closest_center_dist {
+            closest_center_dist = dist;
+        }
+    }
+    n + closest_center_dist
+}
+
+fn spiral_memory_part_two(num: u64) -> u64 {
+    // trash tier solution
+    let LENGTH: usize = 100;
+    let CENTER = LENGTH / 2;
+    let mut grid = [[0; 100]; 100];
+    grid[CENTER][CENTER] = 1;
+    let mut n = 1;
+    let mut i = CENTER;
+    let mut j = CENTER+1;
+    let mut sum_surroundings = 0;
+    while true {
+        // UP N, UP N
+        for _ in 0..(n*2)-1 {
+            sum_surroundings = {
+                grid[i-1][j] + grid[i-1][j-1] + grid[i-1][j+1] + grid[i][j+1] + grid[i][j-1] + grid[i+1][j+1] + grid[i+1][j] + grid[i+1][j-1]
+            };
+            if sum_surroundings > num {
+                return sum_surroundings;
+            }
+            grid[i][j] = sum_surroundings;
+            i -= 1;
+        }
+        // LEFT N, LEFT N
+        for _ in 0..(n*2) {
+            sum_surroundings = {
+                grid[i-1][j] + grid[i-1][j-1] + grid[i-1][j+1] + grid[i][j+1] + grid[i][j-1] + grid[i+1][j+1] + grid[i+1][j] + grid[i+1][j-1]
+            };
+            if sum_surroundings > num {
+                return sum_surroundings;
+            }
+            grid[i][j] = sum_surroundings;
+            j -= 1;
+        }
+        // DOWN N, DOWN N
+        for _ in 0..(n*2) {
+            sum_surroundings = {
+                grid[i-1][j] + grid[i-1][j-1] + grid[i-1][j+1] + grid[i][j+1] + grid[i][j-1] + grid[i+1][j+1] + grid[i+1][j] + grid[i+1][j-1]
+            };
+            if sum_surroundings > num {
+                return sum_surroundings;
+            }
+            grid[i][j] = sum_surroundings;
+            i += 1;
+        }
+        // RIGHT N, RIGHT N
+        for _ in 0..(n*2)+1 {
+            sum_surroundings = {
+                grid[i-1][j] + grid[i-1][j-1] + grid[i-1][j+1] + grid[i][j+1] + grid[i][j-1] + grid[i+1][j+1] + grid[i+1][j] + grid[i+1][j-1]
+            };
+            if sum_surroundings > num {
+                return sum_surroundings;
+            }
+            grid[i][j] = sum_surroundings;
+            j += 1;
+        }
+        n += 1;
+    }
+    10
+}
+
 fn ascii_to_digit(ascii: u8) -> Result<u64, &'static str> {
     match ascii {
         b'0' => Ok(0),
@@ -144,5 +230,14 @@ mod tests {
 9 4 7 3
 3 8 6 5";
         assert_eq!(corruption_checksum_part_two(spreadsheet), Ok(9));
+    }
+
+    #[test]
+    fn test_spiral_memory_part_one() {
+        assert_eq!(spiral_memory_part_one(1), 0);
+        assert_eq!(spiral_memory_part_one(12), 3);
+        assert_eq!(spiral_memory_part_one(22), 3);
+        assert_eq!(spiral_memory_part_one(23), 2);
+        assert_eq!(spiral_memory_part_one(1024), 31);
     }
 }
